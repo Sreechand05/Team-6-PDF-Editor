90efa59a97bc7dcad53f6b2d8c2b09c18da401e8
Sreechand Rayavarapu <142258027+Sreechand05@users.noreply.github.com>
2024-07-08 17:10:49 +0530

    Update PDFViewer.js

--- src/PDFViewer.js
+++ src/PDFViewer.js
@@ -4,8 +4,8 @@ import { Viewer, Worker } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 
 const PDFViewer = ({ file }) => (
-  <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-    <div style={{ height: '750px' }}>
+  <Worker workerUrl="/pdfjs/pdf.worker.min.js">
+    <div style={{height: '560px', width: '44.5%', float:'left'}} className='view'>
       <Viewer fileUrl={file} />
     </div>
   </Worker>
